42032671f7e9a0af4c5eb36cd0e9e7508dccdc81
daikichi86 <maskman-daikigx@i.softbank.jp>
2021-02-22 09:35:49 +0900

    コメント機能実装

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :comment do
-    
+    text { "MyText" }
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+    describe 'コメント機能' do
+      it "コメントが空だと登録できない" do
+        @comment.text = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Text can't be blank")
+      end
+    end
 end
